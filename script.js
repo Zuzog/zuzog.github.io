0d3798f5948cdfa2474e31c22b10600d65176eca
Zuzog <bsft.of@pm.me>
2025-03-12 12:25:07 +0100

    Changed Background Image + Added NoScroll while loading

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const loadingScreen = document.querySelector(".loading-screen");
   const content = document.querySelector(".content");
+  const body = document.body;
   const scrollArrow = document.querySelector(".scroll-arrow");
 
   // Fonction pour faire défiler vers la section
   scrollArrow.addEventListener("click", () => {
-    const targetSection = document.getElementById("skills"); // Remplacez "skills" par l'ID de votre section
+    const targetSection = document.getElementById("skills");
     targetSection.scrollIntoView({ behavior: "smooth" });
   });
 
@@ -24,6 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Wait for resources to load
   window.addEventListener("load", () => {
+    // Add the no-scroll class to block scrolling
+    body.classList.add("no-scroll");
+
     // Delay to see the Lottie animation
     setTimeout(() => {
       // Slide up the loading screen
@@ -32,6 +36,9 @@ document.addEventListener("DOMContentLoaded", () => {
       // Show content
       setTimeout(() => {
         content.classList.add("visible");
+
+        // Remove the no-scroll class to allow scrolling
+        body.classList.remove("no-scroll");
 
         // Animate titles simultaneously
         const titles = document.querySelectorAll(".animated-title");
