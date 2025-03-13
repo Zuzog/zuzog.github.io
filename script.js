e4e4c143cd52f8bfb858d554de2d5d2f463d7297
Zuzog <bsft.of@pm.me>
2025-03-13 15:03:04 +0100

    Style Update, Removed animations

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loadingScreen = document.querySelector(".loading-screen");
-  const content = document.querySelector(".content");
-  const body = document.body;
-  const scrollArrow = document.querySelector(".scroll-arrow");
+const scrollArrow = document.querySelector(".scroll-arrow");
+const content = document.querySelector(".content");
 
-  // Fonction pour faire défiler vers la section
-  scrollArrow.addEventListener("click", () => {
-    const targetSection = document.getElementById("skills");
-    targetSection.scrollIntoView({ behavior: "smooth" });
-  });
+// Fonction pour faire défiler vers la section
+scrollArrow.addEventListener("click", () => {
+  const targetSection = document.getElementById("skills");
+  targetSection.scrollIntoView({ behavior: "smooth" });
+});
 
-  // Function to animate typing effect
-  function typeEffect(element, text, delay) {
-    let index = 0;
-    const interval = setInterval(() => {
-      if (index < text.length) {
-        element.textContent += text.charAt(index);
-        index++;
-      } else {
-        clearInterval(interval);
-      }
-    }, delay);
-  }
+// Function to animate typing effect
+function typeEffect(element, text, delay) {
+  let index = 0;
+  const interval = setInterval(() => {
+    if (index < text.length) {
+      element.textContent += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, delay);
+}
 
-  // Wait for resources to load
-  window.addEventListener("load", () => {
-    // Add the no-scroll class to block scrolling
-    body.classList.add("no-scroll");
-
-    // Delay to see the Lottie animation
-    setTimeout(() => {
-      // Slide up the loading screen
-      loadingScreen.classList.add("slide-up");
-
-      // Show content
-      setTimeout(() => {
-        loadingScreen.classList.add("hidden");
-        content.classList.add("visible");
-
-        // Remove the no-scroll class to allow scrolling
-        body.classList.remove("no-scroll");
-
-        // Animate titles simultaneously
-        const titles = document.querySelectorAll(".animated-title");
-        titles.forEach((title) => {
-          const text = title.textContent;
-          title.textContent = ""; // Clear the title text
-          typeEffect(title, text, 100); // Call the typing effect for each title
-        });
-      }, 1000);
-    }, 2500);
-  });
+// Wait for resources to load
+const titles = document.querySelectorAll(".animated-title");
+titles.forEach((title) => {
+  const text = title.textContent;
+  title.textContent = ""; // Clear the title text
+  typeEffect(title, text, 150); // Call the typing effect for each title
 });
