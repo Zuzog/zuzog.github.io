6024f43d2658049cbbaf0ed3f2d32de3163b4ab4
Zuzog <theo.pierre@keemail.me>
2025-06-17 13:14:07 +0200

    Responsive Improvment - Code Refacto

--- script/script.js
+++ script/script.js
@@ -1,8 +1,40 @@
 // Variables
 const lightSwitch = document.getElementById("light-switch").addEventListener("change", switchLightSwitch);
+const lightSwitchLabel = document.getElementById("light-switch-label");
 const theme = localStorage.getItem('theme');
+const sidebarToggle = document.getElementById("sidebar-button");
+const navToggle = document.getElementById("nav-button");
+const sidebar = document.getElementById("sidebar");
+const nav = document.getElementById("top-nav-items");
+const scrollArrow = document.getElementById("scroll-arrow");
+const sections = document.querySelectorAll(".section");
 
-// Barre de progression
+// Core Functions //
+
+// Light Switch //
+// Get the theme from local storage
+function getTheme() {
+  if (theme === 'dark') {
+    document.documentElement.setAttribute("data-theme", "dark");
+    document.getElementById("light-switch").checked = true;
+  } else {
+    document.documentElement.removeAttribute("data-theme");
+  }
+}
+// Switch the theme
+function switchLightSwitch() {
+  var isChecked = document.getElementById("light-switch").checked;
+  if (isChecked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.removeAttribute("data-theme");
+    localStorage.setItem('theme', 'light');
+  }
+};
+
+// Progress Bar //
+// Update the progress bar
 function updateProgressBar() {
   const winScroll =
     document.body.scrollTop || document.documentElement.scrollTop;
@@ -14,19 +46,18 @@ function updateProgressBar() {
     scrolled / 100
   })`;
 }
-
-// Initialiser la barre de progression à 0
+// Initialize the progress bar to 0
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".progress-bar").style.transform = "scaleY(0)";
 });
-
 window.addEventListener("scroll", updateProgressBar);
 
-// Animation au défilement
+// Animation on scroll //
+// Options for the observer
 const observerOptions = {
   threshold: 0.1,
 };
-
+// Create the observer
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -35,7 +66,7 @@ const observer = new IntersectionObserver((entries) => {
     }
   });
 }, observerOptions);
-
+// Add the animation to the sections
 document.querySelectorAll(".section").forEach((section) => {
   section.style.opacity = "0";
   section.style.transform = "translateY(20px)";
@@ -43,10 +74,11 @@ document.querySelectorAll(".section").forEach((section) => {
   observer.observe(section);
 });
 
-// Défilement fluide au clique sur la navbar
+// Navigation //
+// Smooth scroll on when using the navigation menu
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
     const target = document.querySelector(this.getAttribute("href"));
     if (target) {
       target.scrollIntoView({
@@ -57,45 +89,27 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-// Menu Burger pour les clients mobiles
-const menuToggle = document.querySelector(".menu-toggle");
-const sidebar = document.querySelector(".sidebar");
-
-menuToggle.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-  document.body.classList.toggle("menu-open");
-  updateScrollArrow();
-});
-
-document.addEventListener("click", (e) => {
-  if (!sidebar.contains(e.target) && !menuToggle.contains(e.target)) {
-    sidebar.classList.remove("active");
-    document.body.classList.remove("menu-open");
-    updateScrollArrow();
-  }
-});
-
-// Gestion du déroulement des compétences
+// Skills Section //
+// Management of the skills
 document.querySelectorAll(".skill-category").forEach((category) => {
   category.addEventListener("click", () => {
     const categoryId = category.getAttribute("data-category");
-
-    // Masquer tous les contenus de détail
-    document.querySelectorAll(".skill-detail-content").forEach((content) => {
-      content.classList.remove("active");
-    });
-
-    // Afficher le contenu correspondant
+    // Show/Hide the corresponding content
     const selectedContent = document.querySelector(
       `.skill-detail-content[data-category="${categoryId}"]`
     );
-    selectedContent.classList.add("active");
-
-    // Scroll vers le contenu sélectionné
+    if (selectedContent.classList.contains("active")) {
+      selectedContent.classList.remove("active");
+    } else {
+      document.querySelectorAll(".skill-detail-content").forEach((content) => {
+        content.classList.remove("active");
+      });
+      selectedContent.classList.add("active");
+    }
+    // Scroll to the selected content
     const navHeight = document.querySelector(".top-nav").offsetHeight;
     const selectedContentPosition =
       selectedContent.getBoundingClientRect().top + window.scrollY;
-
     window.scrollTo({
       top: selectedContentPosition - navHeight - 100,
       behavior: "smooth",
@@ -103,10 +117,48 @@ document.querySelectorAll(".skill-category").forEach((category) => {
   });
 });
 
-// Gestion de la flèche de scroll pour les clients mobiles
-const scrollArrow = document.querySelector(".scroll-arrow");
-const sections = document.querySelectorAll(".section");
+// Scroll CTA
+const scrollCta = document.querySelector(".scroll-cta");
+scrollCta.addEventListener("click", () => {
+  const firstSection = document.querySelector("#skills");
+  const navHeight = document.querySelector(".top-nav").offsetHeight;
+  const targetPosition = firstSection.offsetTop - navHeight - 20;
+  window.scrollTo({
+    top: targetPosition,
+    behavior: "smooth",
+  });
+});
 
+
+// Mobile//
+
+// Burger Menu
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  document.body.classList.toggle("sidebar-open");
+  updateScrollArrow();
+});
+document.addEventListener("click", (e) => {
+  if (!sidebar.contains(e.target) && !sidebarToggle.contains(e.target)) {
+    sidebar.classList.remove("active");
+    document.body.classList.remove("sidebar-open");
+    updateScrollArrow();
+  }
+});
+
+
+// Light Switch autohide
+if (window.innerWidth <= 980) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+      lightSwitchLabel.style.display = "none";
+    } else {
+      lightSwitchLabel.style.display = "block";
+    }
+  });
+}
+
+// Scroll arrow
 function updateScrollArrow() {
   if (window.innerWidth <= 768) {
     const currentScroll = window.scrollY;
@@ -114,32 +166,23 @@ function updateScrollArrow() {
     const documentHeight = document.documentElement.scrollHeight;
     const scrollBottom = currentScroll + windowHeight;
 
-    // Si on est en haut de la page, afficher la flèche
-    if (currentScroll < 100 && !document.body.classList.contains("menu-open")) {
+    // Display scroll arrow by default
+    if (!document.body.classList.contains("sidebar-open")) {
       scrollArrow.style.display = "flex";
       return;
     }
 
-    // Si on est proche du bas de la page, masquer la flèche
+    // If we are near the bottom of the page, hide the scroll arrow
     if (scrollBottom >= documentHeight - 100) {
       scrollArrow.style.display = "none";
       return;
     }
 
-    // Trouver la section actuelle
-    let currentSectionIndex = -1;
-    sections.forEach((section, index) => {
-      const rect = section.getBoundingClientRect();
-      if (rect.top <= windowHeight / 2 && rect.bottom >= windowHeight / 2) {
-        currentSectionIndex = index;
-      }
-    });
-
-    // Afficher/masquer la flèche
+    // Show/hide the scroll arrow
     if (
       currentSectionIndex >= 0 &&
       currentSectionIndex < sections.length - 1 &&
-      !document.body.classList.contains("menu-open")
+      !document.body.classList.contains("sidebar-open")
     ) {
       scrollArrow.style.display = "flex";
     } else {
@@ -149,17 +192,15 @@ function updateScrollArrow() {
     scrollArrow.style.display = "none";
   }
 }
-
+// Scroll to the next section
 scrollArrow.addEventListener("click", () => {
   const currentScroll = window.scrollY;
   const windowHeight = window.innerHeight;
-
-  // Si on est en haut de la page, scroll vers la première section
+  // If we are at the top of the page, scroll to the first section
   if (currentScroll < 100) {
     const firstSection = sections[0];
     const navHeight = document.querySelector(".top-nav").offsetHeight;
     const targetPosition = firstSection.offsetTop - navHeight - 20;
-
     window.scrollTo({
       top: targetPosition,
       behavior: "smooth",
@@ -167,7 +208,7 @@ scrollArrow.addEventListener("click", () => {
     return;
   }
 
-  // Trouver la section actuelle
+  // Find the current section
   let currentSectionIndex = -1;
   sections.forEach((section, index) => {
     const rect = section.getBoundingClientRect();
@@ -176,7 +217,7 @@ scrollArrow.addEventListener("click", () => {
     }
   });
 
-  // Scroll vers la section suivante
+  // Scroll to the next section
   if (currentSectionIndex >= 0 && currentSectionIndex < sections.length - 1) {
     const nextSection = sections[currentSectionIndex + 1];
     const navHeight = document.querySelector(".top-nav").offsetHeight;
@@ -189,43 +230,9 @@ scrollArrow.addEventListener("click", () => {
   }
 });
 
-// Appeler updateScrollArrow au chargement de la page
+// Execution / Initialization
 document.addEventListener("DOMContentLoaded", updateScrollArrow);
 window.addEventListener("scroll", updateScrollArrow);
 window.addEventListener("resize", updateScrollArrow);
-
-// Gestion du scroll pour le call to action
-const scrollCta = document.querySelector(".scroll-cta");
-scrollCta.addEventListener("click", () => {
-  const firstSection = document.querySelector("#skills");
-  const navHeight = document.querySelector(".top-nav").offsetHeight;
-  const targetPosition = firstSection.offsetTop - navHeight - 20;
-
-  window.scrollTo({
-    top: targetPosition,
-    behavior: "smooth",
-  });
-});
-
-// Light Switch
-function getTheme() {
-  if (theme === 'dark') {
-    document.documentElement.setAttribute("data-theme", "dark");
-    document.getElementById("light-switch").checked = true;
-  } else {
-    document.documentElement.removeAttribute("data-theme");
-  }
-}
-function switchLightSwitch() {
-  var isChecked = document.getElementById("light-switch").checked;
-  if (isChecked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem('theme', 'dark');
-  } else {
-    document.documentElement.removeAttribute("data-theme");
-    localStorage.setItem('theme', 'light');
-  }
-};
-
 getTheme();
 switchLightSwitch();
